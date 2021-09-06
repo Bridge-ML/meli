32f73dbb544a14af53137ec0698958127bc608cd
Bridge-ML <user.napse@gmail.com>
2021-09-05 22:29:44 -0300

    added clg notifications path

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 
 app.get('/', function (req, res) {
     res.send('working!!')
+    console.log('entro por raiz');
 })
 
 app.get('/notifications', function (req, res) {
     res.send('Notifications endpoint')
+    console.log('entro por notifications');
 })
 app.listen(process.env.PORT || 3000, function () {
     console.log('server listennign on por 3000');
