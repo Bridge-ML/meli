23ce1436d057e3d6cb6ddbb9f911bcad48a8cea7
Bridge-ML <user.napse@gmail.com>
2021-09-05 22:06:47 -0300

    added notifications endpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
     res.send('working!!')
 })
 
+app.get('/notifications', function (req, res) {
+    res.send('Notifications endpoint')
+})
 app.listen(process.env.PORT || 3000, function () {
     console.log('server listennign on por 3000');
 } )
