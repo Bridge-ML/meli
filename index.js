fd94b5b68305d6bcc07632c2f4989f5c4a5f4821
Bridge-ML <user.napse@gmail.com>
2021-09-06 00:45:08 -0300

    added views folder

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ app.get('/notifications', function (req, res) {
     res.send('Notifications endpoint')
     console.log('entro por notifications');
 })
+
+app.get('/posts', async (req, res) => {
+    try {
+      const meliObject = new MeliObject(res.locals.access_token);    
+      const user = await meliObject.get('/users/me');
+      const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
+      if (items.length) {
+        const result = [];
+        const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
+        for await (item of promises) {
+          result.push(item);
+        }
+        res.render('posts', { items: result });
+      } else {
+        res.status(404).send('no items were found :(');
+      }
+    } catch(err) {
+      console.log('Something went wrong', err);
+      res.status(500).send(`Error! ${err}`);
+    }
+  });
+
+
+
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('server listennign on por 3000');
 } )
